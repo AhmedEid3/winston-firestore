92cf63e6a8f112d2f7c52d480bfd155c40aee2b2
AhmedEid3 <ahmed.eid3@outlook.com>
2022-11-01 15:13:48 +0200

    Add WinstonFirestore

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import Transport from "winston-transport";
+
+interface WinstonFirestoreOptions extends Transport.TransportStreamOptions {
+  db: any;
+  collectionPath: string;
+}
+
+export class WinstonFirestore extends Transport {
+  constructor(public options: WinstonFirestoreOptions) {
+    super(options);
+
+    this.validateDB();
+    this.validateCollectionPath();
+  }
+
+  private validateDB() {
+    if (!this.options?.db) {
+      throw new Error("Missing required 'db' in options");
+    }
+  }
+
+  private validateCollectionPath() {
+    if (!this.options?.collectionPath) {
+      throw new Error("Missing required 'collectionPath' in options");
+    }
+
+    const isValidPath = this.options.collectionPath.split("/").length % 2 !== 0;
+    if (!isValidPath) {
+      throw new Error(
+        `Value for argument "collectionPath" must point to a collection,
+             but was "${this.options.collectionPath}". 
+             Your path does not contain an odd number of components.`
+      );
+    }
+  }
+
+  async log(info: any, callback: () => void) {
+    await this.options.db.collection(this.options.collectionPath).add(info);
+
+    callback?.();
+  }
+}
